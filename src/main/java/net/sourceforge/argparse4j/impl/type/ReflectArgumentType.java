eaa56a6e437809331c0003d0847864f841b56678
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2013-02-19 21:33:49 +0900

    ReflectArgumentType: Special handling for enums and remove setAccessible
    Use Enum.valueOf() to get desired enum value from String.
    Calling setAccessible(true) of valueOf method was removed, because
    it may cause some problems, for example, application wants to
    hide valueOf method.

--- src/main/java/net/sourceforge/argparse4j/impl/type/ReflectArgumentType.java
+++ src/main/java/net/sourceforge/argparse4j/impl/type/ReflectArgumentType.java
@@ -28,7 +28,9 @@ public class ReflectArgumentType<T> implements ArgumentType<T> {
      * This object first tries to convert given String using
      * {@code valueOf(java.lang.String)} static method of given {@code type}. If
      * that failed, then use constructor of given {@code type} for conversion.
-     * The constructor of {@code type} must accept 1 String argument.
+     * {@code valueOf()} method and/or constructor must be declared as public.
+     * Otherwise, they cannot be invoked. The constructor of {@code type} must
+     * accept 1 String argument.
      * </p>
      * <p>
      * If error occurred inside the {@code valueOf} static method or
@@ -37,10 +39,10 @@ public class ReflectArgumentType<T> implements ArgumentType<T> {
      * thrown.
      * </p>
      * <p>
-     * Because the enums have {@code valueOf} static method, this object works
-     * with them as well. The enums in its nature have limited number of
-     * members. In {@link #convert(ArgumentParser, Argument, String)}, string
-     * value will be converted to one of them. If it cannot be converted,
+     * This object works with enums as well. The enums in its nature have
+     * limited number of members. In
+     * {@link #convert(ArgumentParser, Argument, String)}, string value will be
+     * converted to one of them. If it cannot be converted,
      * {@link #convert(ArgumentParser, Argument, String)} will throw
      * {@link ArgumentParserException}. This means it already act like a
      * {@link Argument#choices(Object...)}.
@@ -56,6 +58,20 @@ public class ReflectArgumentType<T> implements ArgumentType<T> {
     @Override
     public T convert(ArgumentParser parser, Argument arg, String value)
             throws ArgumentParserException {
+        // Handle enums separately. Enum.valueOf() is very convenient here.
+        // It somehow can access private enum values, where normally T.valueOf()
+        // cannot without setAccessible(true).
+        if (type_.isEnum()) {
+            try {
+                return (T) Enum.valueOf((Class<Enum>) type_, value);
+            } catch (IllegalArgumentException e) {
+                String choices = TextHelper.concat(type_.getEnumConstants(), 0,
+                        ",", "{", "}");
+                throw new ArgumentParserException(String.format(
+                        "could not convert '%s' (choose from %s)", value,
+                        choices), e, parser, arg);
+            }
+        }
         Method m = null;
         try {
             m = type_.getMethod("valueOf", String.class);
@@ -68,11 +84,6 @@ public class ReflectArgumentType<T> implements ArgumentType<T> {
         // Only interested in static valueOf method.
         if (!Modifier.isStatic(m.getModifiers())
                 || !type_.isAssignableFrom(m.getReturnType())) {
-            return convertUsingConstructor(parser, arg, value);
-        }
-        try {
-            m.setAccessible(true);
-        } catch (SecurityException e) {
             return convertUsingConstructor(parser, arg, value);
         }
         Object obj = null;
@@ -110,18 +121,9 @@ public class ReflectArgumentType<T> implements ArgumentType<T> {
     private void throwArgumentParserException(ArgumentParser parser,
             Argument arg, String value, Throwable t)
             throws ArgumentParserException {
-        if (type_.isEnum()) {
-            String choices = TextHelper.concat(type_.getEnumConstants(), 0,
-                    ",", "{", "}");
-            throw new ArgumentParserException(String.format(
-                    "could not convert '%s' (choose from %s)", value, choices),
-                    t, parser, arg);
-
-        } else {
-            throw new ArgumentParserException(String.format(
-                    "could not convert '%s' to %s (%s)", value,
-                    type_.getSimpleName(), t.getMessage()), t, parser, arg);
-        }
+        throw new ArgumentParserException(String.format(
+                "could not convert '%s' to %s (%s)", value,
+                type_.getSimpleName(), t.getMessage()), t, parser, arg);
     }
 
     private void handleInstatiationError(Exception e) {
