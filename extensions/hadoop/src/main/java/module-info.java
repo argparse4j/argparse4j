1334b73f1fd882fac6d6a51981921941712841d7
Concision <contact@concision.me>
2020-09-28 05:58:40 -0700

    #123: integrated experimental JDK9 module-info support

---
+++ extensions/hadoop/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module net.sourceforge.argparse4j.ext.hadoop {
+    requires java.base;
+    requires net.sourceforge.argparse4j;
+    requires hadoop.common;
+
+    exports net.sourceforge.argparse4j.ext.hadoop;
+}
