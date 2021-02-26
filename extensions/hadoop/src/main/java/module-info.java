f6030100f5cd5fe2f34ef699ef6b8206120708bf
Johan Stuyts <j.stuyts@javathinker.com>
2021-02-26 20:54:17 +0100

    #123: Removed module information for Hadoop extension because hadoop-common does not have module information. Publishing with module information could break use of hadoop-common with the Java Module System when hadoop-common chooses a module name.

--- extensions/hadoop/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module net.sourceforge.argparse4j.ext.hadoop {
-    requires java.base;
-    requires net.sourceforge.argparse4j;
-    requires hadoop.common;
-
-    exports net.sourceforge.argparse4j.ext.hadoop;
-}
